dba58ca51517f27eb177dea49c6dc3527198dd09
chenm_youshu <chenm@youshu.cc>
2022-10-28 20:06:37 +0800

    数据合并进行注释

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,24 +1,38 @@
+# coding=utf-8
+'''
+代码注释
+@20221028
+'''
 import pandas as pd
 import numpy as np
 
 def merge_files():
-    #合并点击记录
+    '''
+    合并点击记录
+    '''
+
+    # -----点击数据合并------
     print("merge click files...")
     click_df=pd.read_csv("data/train_preliminary/click_log.csv")
+    # 可以去掉减少数据
     click_df=click_df.append(pd.read_csv("data/train_semi_final/click_log.csv"))
     click_df=click_df.append(pd.read_csv("data/test/click_log.csv"))
-    click_df=click_df.sort_values(by=["time"]).drop_duplicates()   
-    
-    #合并广告信息
+    click_df=click_df.sort_values(by=["time"]).drop_duplicates()
+
+
+    # -----合并广告信息------
     print("merge ad files...")
     ad_df=pd.read_csv("data/train_preliminary/ad.csv")
+    # 可以去掉减少数据
     ad_df=ad_df.append(pd.read_csv("data/train_semi_final/ad.csv"))
     ad_df=ad_df.append(pd.read_csv("data/test/ad.csv"))
     ad_df=ad_df.drop_duplicates() 
     
-    #合并用户信息
+
+    # -----合并用户信息------
     print("merge user files...")
     train_user=pd.read_csv("data/train_preliminary/user.csv")
+    # 可以去掉减少数据
     train_user=train_user.append(pd.read_csv("data/train_semi_final/user.csv"))
     train_user=train_user.reset_index(drop=True)
     train_user['age']=train_user['age']-1
